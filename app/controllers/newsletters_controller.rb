4fa1295d6c2dffb9b989cbd4996d33fb3bc81d75
G-vans <jevansstanley@gmail.com>
2025-09-25 12:49:13 +0300

    Initial rails project setup

---
+++ app/controllers/newsletters_controller.rb
@@ -0,0 +1,17 @@
+class NewslettersController < ApplicationController
+  def index
+    # This will show a list of newsletters (we'll build this later)
+  end
+
+  def new
+    # This will show the editor for creating a new newsletter
+  end
+
+  def create
+    # This will save the newsletter (we'll build this later)
+  end
+
+  def show
+    # This will show a specific newsletter (we'll build this later)
+  end
+end
